9eb6a8e1fa388dd9385be493b734bf88a715b6f4
David-Mateus <david-mateus09@hotmail.com>
2023-04-06 19:43:23 -0300

    tailwind add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <h1>Findy</h1>
+    <>
+     <div class="bg-gray-900">
+     <h1 class="italic text-white">Emmanuel, teste feito com tailwind</h1>
+    </div>
+
+    </>
+   
   )
 }
