ea8466e854225bac259cbc3751175a1370c78702
Lucasgrn <lucasgabriel.ogn@gmail.com>
2023-04-12 13:22:26 -0300

    feat: Integração feed

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -2,7 +2,7 @@ import { Post } from "../../../models/Post";
 
 export default async function View(req, res) {
   try {
-    const post = await Post.find()
+    const post = await Post.find().populate('user')
     res.status(200).send(post)
   } catch (error) {
     res.status(500).send({ error: error })
